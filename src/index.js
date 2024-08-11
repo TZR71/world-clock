dffab0e4bce3fbe1c22ad68268ac1e3442757940
TZR71 <Tiinarojola@gmail.com>
2024-08-11 19:30:41 +0200

    added functions, played with css, inserted rest of JS

--- src/index.js
+++ src/index.js
@@ -34,3 +34,23 @@ setInterval(function () {
     "hh:mm:ss [<small>]A[</small>]"
   )} `;
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("HH:mm")} </div>
+  </div>
+  `;
+}
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
